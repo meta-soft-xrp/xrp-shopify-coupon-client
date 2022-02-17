febfa1b660fdc8ccaf25c5256e9b453500e0679b
Siddharth Ramesh <siddharthramesh@Siddharths-MacBook-Pro.local>
2022-02-17 13:30:42 +0530

    WIP

--- .netlify/functions/csp.js
+++
@@ -1,6 +0,0 @@
-exports.handler = async function(event, context) {
-	console.log(event.headers)
-	return {
-		headers: { "Content-Security-Policy": "frame-ancestors https://shop.myshopify.com https://admin.shopify.com" }
-	}
-}
