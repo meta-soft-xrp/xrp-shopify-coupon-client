14b7a30ad0e3a0b8723726e4fda2117aacc2844a
jithendra43 <jithendra43@gmail.com>
2022-10-03 17:36:12 +0530

    Transaction details loading added

--- src/routes/transaction/index.js
+++ src/routes/transaction/index.js
@@ -12,7 +12,7 @@ import {
   VStack,
   Text,
   Divider,
-  useColorMode,
+  SkeletonText,
   Link,
 } from "@chakra-ui/react";
 import { useContext, useEffect, useState } from "react";
@@ -33,71 +33,84 @@ const TransactionRoute = () => {
     getTransactionState(shop);
   }, []);
 
-  
-
-  return (
-    <>
-      
-      <Container
-        maxW={"7xl"}
-        p={[12, 6]}
-        h="100vh"
-        bg="#f6f6f7"
-        textAlign={"left"}
-      >
-        <Box bg="white" width={"5xl"} m="auto" p={5} borderRadius="10px">
-          <VStack spacing={2} align="stretch">
-            <Box>
-              <Text size="xl" fontWeight="bold">
-                XRP Transaction Details
-              </Text>
-              <Divider borderColor="gray.200" />
-            </Box>
-          </VStack>
-          {/* {transactionState.get.success.data?.result?.transactions.map((details) => (
-           <Text>{details.tx.Account}</Text>
-         ))} */}
-          <TableContainer p="5">
-            <Table variant={"simple"}>
-              <Thead>
-                <Tr>
-                  <Th>Account</Th>
-                  <Th isNumeric>Amount</Th>
-                  <Th>Ledger Index</Th>
-                  <Th>Fee</Th>
-                  <Th>Transaction Ref</Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {transactionState.get.success.data?.result?.transactions.map(
-                  (details) => (
-                    // <Text>{details.tx.Account}</Text>
-                    <Tr>
-                      <Td>{details.tx.Account}</Td>
-                      <Td isNumeric>
-                        {window.xrpl.dropsToXrp(details.tx.Amount)}
-                      </Td>
-                      <Td>{details.tx.inLedger}</Td>
-                      <Td>{details.tx.Fee}</Td>
-                      <Td>
-                        <Link
-                          color="teal"
-                          target="_blank"
-                          href={`${process.env.REACT_APP_XRP_TRANSACTION_REFFERENCE}transactions/${details.tx.hash}`}
-                        >
-                          {details.tx.hash}
-                        </Link>
-                      </Td>
-                    </Tr>
-                  )
-                )}
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </Box>
-      </Container>
-    </>
-  );
+  if (transactionState.get.success.data.length === 0) {
+    return (
+      <>
+        <NavBar />
+        <Container
+          maxW={"7xl"}
+          p={[12, 6]}
+          minH={"100vh"}
+          bg="#f6f6f7"
+          textAlign={"left"}
+        >
+          <Box padding="6" boxShadow="lg" bg="white">
+            <SkeletonText mt="4" noOfLines={4} spacing="4" />
+          </Box>
+        </Container>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <NavBar />
+        <Container
+          maxW={"7xl"}
+          p={[12, 6]}
+          bg="#f6f6f7"
+          textAlign={"left"}
+        >
+          <Box bg="white" width={"5xl"} m="auto" p={5} borderRadius="10px">
+            <VStack spacing={2} align="stretch">
+              <Box>
+                <Text size="xl" fontWeight="bold">
+                  XRP Transaction Details
+                </Text>
+                <Divider borderColor="gray.200" />
+              </Box>
+            </VStack>
+            <TableContainer p="5">
+              <Table variant={"simple"}>
+                <Thead>
+                  <Tr>
+                    <Th>Account</Th>
+                    <Th isNumeric>Amount</Th>
+                    <Th>Ledger Index</Th>
+                    <Th>Fee</Th>
+                    <Th>Transaction Ref</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {transactionState.get.success.data?.result?.transactions.map(
+                    (details) => (
+                      // <Text>{details.tx.Account}</Text>
+                      <Tr>
+                        <Td>{details.tx.Account}</Td>
+                        <Td isNumeric>
+                          {window.xrpl.dropsToXrp(details.tx.Amount)}
+                        </Td>
+                        <Td>{details.tx.inLedger}</Td>
+                        <Td>{details.tx.Fee}</Td>
+                        <Td>
+                          <Link
+                            color="teal"
+                            target="_blank"
+                            href={`${process.env.REACT_APP_XRP_TRANSACTION_REFFERENCE}transactions/${details.tx.hash}`}
+                          >
+                            {details.tx.hash}
+                          </Link>
+                        </Td>
+                      </Tr>
+                    )
+                  )}
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </Box>
+        </Container>
+      </>
+    );
+  }
 };
 
 export default TransactionRoute;
